21be403db922e9814c01c646bbc1d75dc5aac9f4
sayebadiane <sayesyll21@gmail.com>
2022-12-08 15:04:55 +0000

    connexion de spring boot avec mysql

---
+++ src/main/java/com/example/projet1_spring_boot/Projet1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet1_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet1SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet1SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet1_spring_boot/Projet1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet1_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet1SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
